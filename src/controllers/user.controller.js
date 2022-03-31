e366bf38f1697362baf025d8954aed21f4e8cb18
KAJAL GANORKAR <kajalganorkar97@gmail.com>
2022-03-31 12:07:27 +0530

    make it better

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ const express = require('express');
 const User = require("../models/user.model");
 const router = express.Router();
 
-router.get("/users", async (req, res) => {
+router.get("", async (req, res) => {
     try {
         let users = await User.find().lean().exec();
         return res.status(200).send(users);
@@ -11,7 +11,7 @@ router.get("/users", async (req, res) => {
     }
 });
 
-router.post("/user", async (req, res) => {
+router.post("", async (req, res) => {
     try {
         const user = await User.create(req.body);
         res.send(user);
